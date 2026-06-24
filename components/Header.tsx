04307606b41896a192efa7580b1c1d6c5429548d
chithraksha16 <chithrakshakharvi@gmail.com>
2026-06-24 20:52:31 +0530

    updated header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,10 @@
 
 const Header = () => {
   return (
-    <div>
-      Header
+    <div className="w-full h-20 flex items-center bgblack text-white shadow-lg">
+      <nav className="w-full">
+      <h3>Tatto<span>Star</span></h3>
+      </nav>
     </div>
   )
 }
