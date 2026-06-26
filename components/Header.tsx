7fe1a741879eba4289abd5a2a861f218ae1f7dd8
chithraksha16 <chithrakshakharvi@gmail.com>
2026-06-26 20:56:02 +0530

    changed white to black theme

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,10 +2,10 @@ import Button from "./ui/Button"
 
 const Header = () => {
   return (
-    <div className="w-full h-20 flex items-center bgblack text-white shadow-lg">
-      <nav className="w-full px-10 flex justify-between">
-      <h3 className="text-2xl text-black font-black">Tatto<span className="font-serif font-medium">Star</span></h3>
-      <ul className="list-none flex gap-5 text-black text-base font-medium">
+    <div className="w-full h-20 flex items-center  text-white shadow-lg">
+      <nav className="w-full px-10 flex justify-between items-center">
+      <h3 className="text-2xl text-white font-black">Tatto<span className="font-serif font-medium">Star</span></h3>
+      <ul className="list-none flex gap-5 text-white text-base font-medium">
         <li>Home</li>
         <li>Our Services</li>
         <li>About Us</li>
