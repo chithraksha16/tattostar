11b916bbde78d402228e88b8754c915d7466dd2f
chithraksha16 <chithrakshakharvi@gmail.com>
2026-06-29 20:50:47 +0530

    border added to header

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,7 @@ import Button from "./ui/Button"
 
 const Header = () => {
   return (
-    <div className="w-full h-20 flex items-center bg-black text-white shadow-lg">
+    <div className="w-full h-20 flex items-center bg-black text-white shadow-lg border-b-2 border-white ">
       <nav className="w-full px-10 flex justify-between items-center">
       <h3 className="text-2xl text-white font-black">Tatto<span className="font-serif font-medium">Star</span></h3>
       <ul className="list-none flex gap-5 text-white text-base font-medium">
